c8df6656da52596f216612104345a8be2ef581ae
Boris <borismjimenezj@gmail.com>
2023-08-24 23:30:57 -0400

    se concluyo la pagina de inicio

--- assets/main.js
+++ assets/main.js
@@ -6,4 +6,53 @@ hamburguesaBtn.addEventListener('click', () => {
 });
 
 
+/*const altura = 170;
+const peso = 97;
+const edad = 33;
+const factor = 1.375;
+
+function imc (altura, peso) {
+    let imc = Math.round(peso / ((altura/100)**2));
+    return console.log(imc)
+}
+
+function calorias (altura, peso, edad, factor) {
+    let calorias = Math.round((655 + (9.6*peso))+((1,8*altura)-(4.7*edad))*factor)
+    return console.log(calorias)
+}
+
+imc(altura, peso)
+calorias(altura, peso, edad, factor)*/
+
+const alturaInput = document.querySelector('#estatura');
+const pesoInput = document.querySelector('#peso');
+const edadInput = document.querySelector('#edad');
+const imcOutput = document.querySelectorAll('.calculadora-respuesta')[0];
+const caloriasOutput = document.querySelectorAll('.calculadora-respuesta')[1];
+const factor = 1.375;
+
+const calcularButton = document.querySelector('#calculadora');
+
+calcularButton.addEventListener('click', () => {
+    event.preventDefault();
+    const altura = parseFloat(alturaInput.value);
+    const peso = parseFloat(pesoInput.value);
+    const edad = parseFloat(edadInput.value);
+
+    const imcResult = imc(altura, peso);
+    const caloriasResult = calorias(altura, peso, edad, factor);
+
+    imcOutput.value = `Tu Indice de Masa Corporal es: ${imcResult}`;
+    caloriasOutput.value = `Las Calorias a consumir son: ${caloriasResult}`;
+});
+
+function imc (altura, peso) {
+    let imc = Math.round(peso / ((altura/100)**2));
+    return imc
+}
+
+function calorias (altura, peso, edad, factor) {
+    let calorias = Math.round((655 + (9.6*peso))+((1,8*altura)-(4.7*edad))*factor)
+    return calorias
+}
 
